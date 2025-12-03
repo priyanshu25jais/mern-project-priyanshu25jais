cf5728b1075be36d3f89df9d4c49dde0a4f7e601
priyanshu25jais <jais.priyanshu25@gmail.com>
2025-12-03 15:16:09 +0530

    change

--- Mentor_mentee_portal/Backend/config/db.js
+++ Mentor_mentee_portal/Backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB connected successfully");
+    console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
     process.exit(1);
--- Mentor_mentee_portal/Backend/index.js
+++ Mentor_mentee_portal/Backend/index.js
@@ -34,5 +34,5 @@ app.use((err, req, res, next) => {
   
 const PORT = process.env.PORT || 4001;
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
